556d6d012c6c7c20bb292307b93244097b7d37b5
jiwondh <win6ter@naver.com>
2017-10-29 11:34:16 +0900

    favicon 추가

--- routes/api.js
+++ routes/api.js
@@ -28,5 +28,40 @@ module.exports = function(app) {
       }
     });
   });
+
+  router.get('/datetime', function(req, res, next) {
+    var q = req.query.q;
+    client.search({
+      index: 'tab',
+      type: 'page',
+      body: {
+        sort : "_timestamp",
+        size : 10000,
+        fields: ["_timestamp", "_source"],
+        query: {
+          match: {
+            "DateTime": {
+              "query": q,
+              "operator": "or",
+              "minimum_should_match": 10,
+              "fuzziness": "AUTO"
+            }
+          }
+        },
+      }
+    }, function(error, response, status) {
+      if (error) {
+        console.log("search error: " + error)
+      } else {
+        console.log("--- Response ---");
+        console.log(response);
+        console.log("--- Hits ---");
+        console.log(response.hits.hits)
+        var total = response.hits.total;
+        res.send(JSON.stringify(response.hits.hits))
+      }
+    });
+  });
+
   return router;
 }
